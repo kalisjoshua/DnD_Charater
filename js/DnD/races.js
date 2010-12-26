709a3990fb54546d3824f0d64237bf0ddc72d6ec
Joshua T Kalis <kalisjoshua@gmail.com>
2010-12-26 12:30:04 -0500

    relocated langs function into utility object

--- js/DnD/races.js
+++ js/DnD/races.js
@@ -1,20 +1,10 @@
 //// races.js
-
-var langs = function (ar) {
-        var result = [];
-    
-        do {
-            result.push(prv.languages[ar[0]]);
-        } while (ar.shift());
-    
-        return result;
-    };
     
 prv.races = {
     "Dwarf": {
     	Abilities   : [0, 10, 15, 0, 0, 0, -10, -5],
     	Infravision : 60,
-    	Languages   : langs([4, 5, 8, 9]),
+    	Languages   : prv.util.langs([4, 5, 8, 9]),
     	Move        : 6,
     	Stats       : [0, 0, 0, 0, 1, -1, 0],
     	SavesBonus  : "+1 on saves(rsw, sp, poison) for each 3 1/2 of con"
@@ -23,7 +13,7 @@ prv.races = {
     "Elf": {
     	Abilities   : [5, -5, 0, 5, 10, 5, 0, 0],
     	Infravision : 60,
-    	Languages   : langs([3, 4, 5, 6, 7, 9]),
+    	Languages   : prv.util.langs([3, 4, 5, 6, 7, 9]),
     	Move        : 12,
     	Stats       : [0, 0, 0, 1, -1, 0, 0],
     	SavesBonus  : ""
@@ -32,7 +22,7 @@ prv.races = {
     "Gnome": {
     	Abilities   : [0, 5, 10, 5, 5, 10, 15, 0],
     	Infravision : 60,
-    	Languages   : langs([0, 1, 6, 5, 8]),
+    	Languages   : prv.util.langs([0, 1, 6, 5, 8]),
     	Move        : 6,
     	Stats       : [-1, 0, 0, 1, 0, 0, 0],
     	SavesBonus  : "+1 on saves(rsw, sp) for each 3 1/2 of con"
@@ -41,7 +31,7 @@ prv.races = {
     "Goblin": {
     	Abilities   : [ 0, 15, 10, 0, 0, 15, 0, 15],
     	Infravision : 30,
-    	Languages   : langs([1, 3, 7, 8]),
+    	Languages   : prv.util.langs([1, 3, 7, 8]),
     	Move        : 8,
     	Stats       : [-1, 1, 0, 1, 0, -1, 0],
     	SavesBonus  : ""
@@ -50,7 +40,7 @@ prv.races = {
     "Half-Elf": {
     	Abilities   : [10, 0, 0, 5, 0, 0, 0, 0],
     	Infravision : 60,
-    	Languages   : langs([3, 4, 5, 6, 7, 9]),
+    	Languages   : prv.util.langs([3, 4, 5, 6, 7, 9]),
     	Move        : 12,
     	Stats       : [0, 0, 0, 0, 0, 0, 0],
     	SavesBonus  : ""
@@ -59,7 +49,7 @@ prv.races = {
     "Half-Orc": {
     	Abilities   : [ -5, 5, 5, 0, 0, 5, 5, -10],
     	Infravision : 60,
-    	Languages   : langs([9]),
+    	Languages   : prv.util.langs([9]),
     	Move        : 12,
     	Stats       : [1, 0, 0, 0, 1, -1, 0],
     	SavesBonus  : ""
@@ -68,7 +58,7 @@ prv.races = {
     "Halfling": {
     	Abilities   : [ 5, 5, 5, 10, 15, 5, -15, -5],
     	Infravision : 30,
-    	Languages   : langs([1, 2, 4, 5, 9]),
+    	Languages   : prv.util.langs([1, 2, 4, 5, 9]),
     	Move        : 6,
     	Stats       : [-1, 0, 0, 1, 0, 0, 0],
     	SavesBonus  : "+1 on saves(rsw, sp, poison) for each 3 1/2 of con"
@@ -77,7 +67,7 @@ prv.races = {
     "Human": {
     	Abilities   : [0, 0, 0, 0, 0, 0, 0, 0],
     	Infravision : 0,
-    	Languages   : langs([]),
+    	Languages   : prv.util.langs([]),
     	Move        : 12,
     	Stats       : [0, 0, 0, 0, 0, 0, 0],
     	SavesBonus  : ""
