9d26084fd8ff16c2adcfc16632175d1cda47b825
Joshua T Kalis <kalisjoshua@gmail.com>
2010-12-27 10:32:24 -0500

    added raceName to the objects

--- js/DnD/races.js
+++ js/DnD/races.js
@@ -2,75 +2,83 @@
     
 prv.races = {
     "Dwarf": {
-    	Abilities   : [0, 10, 15, 0, 0, 0, -10, -5],
-    	Infravision : 60,
-    	Languages   : prv.util.langs([4, 5, 8, 9]),
-    	Move        : 6,
-    	Stats       : [0, 0, 0, 0, 1, -1, 0],
-    	SavesBonus  : "+1 on saves(rsw, sp, poison) for each 3 1/2 of con"
+    	abilities   : [0, 10, 15, 0, 0, 0, -10, -5],
+    	infravision : 60,
+    	languages   : prv.util.langs([4, 5, 8, 9]),
+    	move        : 6,
+    	raceName    : "Dwarf",
+    	stats       : [0, 0, 0, 0, 1, -1, 0],
+    	savesBonus  : "+1 on saves(rsw, sp, poison) for each 3 1/2 of con"
     },
     
     "Elf": {
-    	Abilities   : [5, -5, 0, 5, 10, 5, 0, 0],
-    	Infravision : 60,
-    	Languages   : prv.util.langs([3, 4, 5, 6, 7, 9]),
-    	Move        : 12,
-    	Stats       : [0, 0, 0, 1, -1, 0, 0],
-    	SavesBonus  : ""
+    	abilities   : [5, -5, 0, 5, 10, 5, 0, 0],
+    	infravision : 60,
+    	languages   : prv.util.langs([3, 4, 5, 6, 7, 9]),
+    	move        : 12,
+    	raceName    : "Elf",
+    	stats       : [0, 0, 0, 1, -1, 0, 0],
+    	savesBonus  : ""
     },
     
     "Gnome": {
-    	Abilities   : [0, 5, 10, 5, 5, 10, 15, 0],
-    	Infravision : 60,
-    	Languages   : prv.util.langs([0, 1, 6, 5, 8]),
-    	Move        : 6,
-    	Stats       : [-1, 0, 0, 1, 0, 0, 0],
-    	SavesBonus  : "+1 on saves(rsw, sp) for each 3 1/2 of con"
+    	abilities   : [0, 5, 10, 5, 5, 10, 15, 0],
+    	infravision : 60,
+    	languages   : prv.util.langs([0, 1, 6, 5, 8]),
+    	move        : 6,
+    	raceName    : "Gnome",
+    	stats       : [-1, 0, 0, 1, 0, 0, 0],
+    	savesBonus  : "+1 on saves(rsw, sp) for each 3 1/2 of con"
     },
     
     "Goblin": {
-    	Abilities   : [ 0, 15, 10, 0, 0, 15, 0, 15],
-    	Infravision : 30,
-    	Languages   : prv.util.langs([1, 3, 7, 8]),
-    	Move        : 8,
-    	Stats       : [-1, 1, 0, 1, 0, -1, 0],
-    	SavesBonus  : ""
+    	abilities   : [ 0, 15, 10, 0, 0, 15, 0, 15],
+    	infravision : 30,
+    	languages   : prv.util.langs([1, 3, 7, 8]),
+    	move        : 8,
+    	raceName    : "Goblin",
+    	stats       : [-1, 1, 0, 1, 0, -1, 0],
+    	savesBonus  : ""
     },
     
     "Half-Elf": {
-    	Abilities   : [10, 0, 0, 5, 0, 0, 0, 0],
-    	Infravision : 60,
-    	Languages   : prv.util.langs([3, 4, 5, 6, 7, 9]),
-    	Move        : 12,
-    	Stats       : [0, 0, 0, 0, 0, 0, 0],
-    	SavesBonus  : ""
+    	abilities   : [10, 0, 0, 5, 0, 0, 0, 0],
+    	infravision : 60,
+    	languages   : prv.util.langs([3, 4, 5, 6, 7, 9]),
+    	move        : 12,
+    	raceName    : "Half-Elf",
+    	stats       : [0, 0, 0, 0, 0, 0, 0],
+    	savesBonus  : ""
     },
     
     "Half-Orc": {
-    	Abilities   : [ -5, 5, 5, 0, 0, 5, 5, -10],
-    	Infravision : 60,
-    	Languages   : prv.util.langs([9]),
-    	Move        : 12,
-    	Stats       : [1, 0, 0, 0, 1, -1, 0],
-    	SavesBonus  : ""
+    	abilities   : [ -5, 5, 5, 0, 0, 5, 5, -10],
+    	infravision : 60,
+    	languages   : prv.util.langs([9]),
+    	move        : 12,
+    	raceName    : "Half-Orc",
+    	stats       : [1, 0, 0, 0, 1, -1, 0],
+    	savesBonus  : ""
     },
     
     "Halfling": {
-    	Abilities   : [ 5, 5, 5, 10, 15, 5, -15, -5],
-    	Infravision : 30,
-    	Languages   : prv.util.langs([1, 2, 4, 5, 9]),
-    	Move        : 6,
-    	Stats       : [-1, 0, 0, 1, 0, 0, 0],
-    	SavesBonus  : "+1 on saves(rsw, sp, poison) for each 3 1/2 of con"
+    	abilities   : [ 5, 5, 5, 10, 15, 5, -15, -5],
+    	infravision : 30,
+    	languages   : prv.util.langs([1, 2, 4, 5, 9]),
+    	move        : 6,
+    	raceName    : "Halfling",
+    	stats       : [-1, 0, 0, 1, 0, 0, 0],
+    	savesBonus  : "+1 on saves(rsw, sp, poison) for each 3 1/2 of con"
     },
     
     "Human": {
-    	Abilities   : [0, 0, 0, 0, 0, 0, 0, 0],
-    	Infravision : 0,
-    	Languages   : prv.util.langs([]),
-    	Move        : 12,
-    	Stats       : [0, 0, 0, 0, 0, 0, 0],
-    	SavesBonus  : ""
+    	abilities   : [0, 0, 0, 0, 0, 0, 0, 0],
+    	infravision : 0,
+    	languages   : prv.util.langs([]),
+    	move        : 12,
+    	raceName    : "Human",
+    	stats       : [0, 0, 0, 0, 0, 0, 0],
+    	savesBonus  : ""
     }
 };
 
