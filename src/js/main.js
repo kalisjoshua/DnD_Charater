3781268162d2951ec5d445dc2009c1f33ea63464
Joshua T Kalis <kalisjoshua@gmail.com>
2013-02-16 21:45:03 -0500

    added basic config for main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,23 @@
-/*jshint */
+/*jshint laxcomma:true*/
 /*global require define*/
 
-require.config({});
+require.config({
+  basePath: "js"
+  ,paths: {
+    handlebars: "lib/handlebars"
+    ,jquery: ["//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min", "lib/jquery"]
+  }
+  ,shim: {
+    handlebars: {
+      exports: "handlebars"
+    }
+  }
+});
+
+define(["jquery"], function ($) {
+  "use strict";
+  
+  $.fn.ready(function () {
+    $("body").css("background", "#BADA55");
+  });
+});
